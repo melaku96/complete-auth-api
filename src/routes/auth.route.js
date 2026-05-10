0744487ca2270a06545346413365dcb99d19bea7
Melaku Belew <embd2026gc@gmail.com>
2026-05-10 23:08:24 +0300

    configure email resend and verification endpoints

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { authLimiter } from "../middlewares/rateLimit.middleware.js";
-import { registerController } from "../controllers/auth.controller.js";
+import { registerController, resendVerificationController, verificationController } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 //routes
 router.post("/register", authLimiter, registerController);
+router.post("/resend-verification", authLimiter, resendVerificationController);
+router.get("/verify-email", verificationController);
 
 export default router;
