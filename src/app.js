1ef8c3e8677f1f494e678d473755e2d7dd7556f0
Melaku Belew <embd2026gc@gmail.com>
2026-05-10 22:57:42 +0300

    configure auth router and apiLimiter middleware

--- src/app.js
+++ src/app.js
@@ -4,18 +4,21 @@ import helmet from "helmet";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import { globalErrorHandler } from "./middlewares/globalErrorHandler.js";
+import authRoute from "./routes/auth.route.js";
+import { apiLimiter } from "./middlewares/rateLimit.middleware.js";
 
 const app = express();
 
 //Middleware
 app.use(helmet());
 app.use(cors());
+app.use(apiLimiter);
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
 
 //Routes
-
+app.use("/api/auth", authRoute);
 //404 handling
 app.use((req, res)=>{
   res.status(404).json({
