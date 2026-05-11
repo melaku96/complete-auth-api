8f86d361ef923f47756451821672745db4995fc0
Melaku Belew <embd2026gc@gmail.com>
2026-05-11 21:19:09 +0300

    configure routes endpoint

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { authLimiter } from "../middlewares/rateLimit.middleware.js";
-import { registerController, resendVerificationController, verificationController } from "../controllers/auth.controller.js";
+import { forgotPasswordController, loginController, registerController, resendVerificationController, resetPasswordController, verificationController } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
@@ -8,5 +8,8 @@ const router = express.Router();
 router.post("/register", authLimiter, registerController);
 router.post("/resend-verification", authLimiter, resendVerificationController);
 router.get("/verify-email", verificationController);
+router.post("/login", loginController);
+router.post("/forgot-password", forgotPasswordController);
+router.patch("/reset-password/:token", resetPasswordController);
 
 export default router;
