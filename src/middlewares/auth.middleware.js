a0b29316a6fe18b87bb38f3ba7dffd0661c38475
Melaku Belew <embd2026gc@gmail.com>
2026-05-12 00:10:49 +0300

    add authentication middleware

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,18 @@
+import userModel from "../models/user.model.js";
+import ApiError from "../utils/ApiError.js";
+import { catchAsync } from "../utils/catchAsync.js";
+
+export const authMiddleware = catchAsync(async(req, res, next)=>{
+  const token = req.cookies.accessToken;
+  if(!token){
+    throw new ApiError("Token not found", 400);
+  };
+  const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+  const user = await userModel.findOne({_id:decoded._id});
+  if(!user){
+    throw new ApiError("User no longer exist", 404);
+  }
+
+  req.user = decoded;
+  next();
+});
