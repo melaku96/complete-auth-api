13cf20e8d24fce9e62c4020623042256f48469c8
Melaku Belew <embd2026gc@gmail.com>
2026-05-11 21:41:26 +0300

    add refresh token service

---
+++ src/services/refresh.token.service.js
@@ -0,0 +1,24 @@
+import userModel from "../models/user.model.js";
+import ApiError from "../utils/ApiError.js"
+import { generateToken } from "../utils/generateToken.js";
+import { cryptoHash } from "../utils/hashToken.js";
+import crypto from "crypto";
+
+export const refreshTokenService = async(token)=>{
+  if(!token){
+    throw new ApiError("Token not found", 4013);
+  }
+  const hashedToken = cryptoHash(token);
+  const user = await userModel.findOne({refreshToken: hashedToken});
+  if(!user){
+    throw new ApiError("Invalid or expired token", 403);
+  }
+  const newAccessToken = generateToken(user);
+  const newRefreshToken = crypto.randomBytes(32).toString('hex');
+  const hashedNewRefreshToken = cryptoHash(newRefreshToken);
+
+  user.refreshToken = hashedNewRefreshToken;
+  await user.save();
+
+  return {newAccessToken, newRefreshToken};
+};
