0579ae09c8b48332bf3fd4f4b75828678391a3e2
Melaku Belew <embd2026gc@gmail.com>
2026-05-11 21:49:14 +0300

    implement token refresh controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { forgotPasswordService } from "../services/forgot.password.service.js";
 import { loginService } from "../services/login.service.js";
 import { logoutService } from "../services/logout.service.js";
+import { refreshTokenService } from "../services/refresh.token.service.js";
 import { registerService } from "../services/register.service.js";
 import { resendVerificationService } from "../services/resend.verification.service.js";
 import { resetPasswordService } from "../services/reset.password.service.js";
@@ -61,20 +62,20 @@ export const loginController = catchAsync(async (req, res) => {
     });
 });
 //FORGOT CONTROLLER***********
-export const forgotPasswordController = catchAsync(async(req, res)=>{
-    const {email} = req.body;
-    const {resetURL} = await forgotPasswordService(email);
+export const forgotPasswordController = catchAsync(async (req, res) => {
+    const { email } = req.body;
+    const { resetURL } = await forgotPasswordService(email);
 
     res.status(200).json({
-        success:true,
-        message:'Reset URL just sent for you',
+        success: true,
+        message: 'Reset URL just sent for you',
         resetURL
     });
 });
 //RESET PASSWORD CONTROLLER
-export const resetPasswordController = catchAsync(async(req,res)=>{
-    const {password} = req.body;
-    const {token} = req.params;
+export const resetPasswordController = catchAsync(async (req, res) => {
+    const { password } = req.body;
+    const { token } = req.params;
 
     await resetPasswordService(password, token);
 
@@ -83,21 +84,44 @@ export const resetPasswordController = catchAsync(async(req,res)=>{
         message: "Your password is reseted successfully",
     });
 })
-//LOGOUT CONTROLLER
-export const logoutController = catchAsync(async(req, res)=>{
-    const token = req.cookies.refreshToken;
-    await logoutService(token);
-      res.clearCookie('accessToken',{
-        httpOnly: true, 
+//REFRESH TOKEN
+export const refreshTokenController = catchAsync(async (req, res) => {
+    const token = req.cookies.token;
+    const { newAccessToken, newRefreshToken } = await refreshTokenService(token);
+    //set cookies
+    res.cookie('accessToken', newAccessToken, {
+        httpOnly: true,
         secure: false,
-        sameSite: 'strict', 
+        sameSite: 'strict',
         maxAge: 30 * 60 * 1000,
     });
-    res.clearCookie('refreshToken',{
-        httpOnly: true, 
+    res.cookie('refreshToken', newRefreshToken, {
+        httpOnly: true,
         secure: false,
-        sameSite: 'strict', 
-        maxAge: 7*24*60*60*1000,
+        sameSite: 'strict',
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    res.status(200).json({
+        success: true,
+        message: 'Token refreshed successfully!',
+    });
+})
+//LOGOUT CONTROLLER
+export const logoutController = catchAsync(async (req, res) => {
+    const token = req.cookies.refreshToken;
+    await logoutService(token);
+    res.clearCookie('accessToken', {
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict',
+        maxAge: 30 * 60 * 1000,
+    });
+    res.clearCookie('refreshToken', {
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict',
+        maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
     res.status(200).json({
