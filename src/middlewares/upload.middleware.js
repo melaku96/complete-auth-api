0057bc13685f049d6884ab483b0df9f0d3442570
Melaku Belew <embd2026gc@gmail.com>
2026-05-12 00:25:25 +0300

    refactor file destinetion directory

--- src/middlewares/upload.middleware.js
+++ src/middlewares/upload.middleware.js
@@ -2,7 +2,7 @@ import multer from 'multer';
 import path from 'path'
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb)=> cb(null, 'src/uploads/'),
+    destination: (req, file, cb)=> cb(null, 'uploads/'),
     filename: (req, file, cb)=> cb(null, Date.now()+ path.extname(file.originalname)),
 });
 
