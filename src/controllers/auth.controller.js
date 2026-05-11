9c131dcf101642328bf1b53f35ba9f21fe21cb7e
Melaku Belew <embd2026gc@gmail.com>
2026-05-11 19:28:45 +0300

    implement user login controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import { loginService } from "../services/login.service.js";
 import { registerService } from "../services/register.service.js";
 import { resendVerificationService } from "../services/resend.service.js";
 import { verificationService } from "../services/verification.service.js";
@@ -14,8 +15,8 @@ export const registerController = catchAsync(async (req, res) => {
     });
 });
 //RESEND VERIFICATION CONTROLLER***********
-export const resendVerificationController = catchAsync(async(req, res)=>{
-    const {email} = req.body;
+export const resendVerificationController = catchAsync(async (req, res) => {
+    const { email } = req.body;
     await resendVerificationService(email);
 
     res.json({
@@ -30,5 +31,29 @@ export const verificationController = catchAsync(async (req, res) => {
     res.status(200).json({
         success: true,
         message: "Email Verified Successfully!"
+    });
+});
+//USER LOGIN CONTROLLER ****************
+export const loginController = catchAsync(async (req, res) => {
+    const { email, password } = req.body;
+    const { accessToken, refreshToken, user } = await loginService(email, password);
+    //set cookies
+    res.cookie('accessToken', accessToken, {
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict',
+        maxAge: 15 * 60 * 1000,
+    });
+    res.cookie('refreshToken', refreshToken, {
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict',
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    res.status(200).json({
+        success: true,
+        message: 'Login Successfully!',
+        user
     });
 });
