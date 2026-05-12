3ca0849e4b7ca13f0eacaceb5ebf432ec8cb8137
Melaku Belew <embd2026gc@gmail.com>
2026-05-12 20:44:41 +0300

    migrate profile upload endpoint from auth to user route

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,6 +1,7 @@
 import express from "express";
-import { changePasswordController, deleteUserController, getCurrentUserController, updateUserController } from "../controllers/user.controller.js";
+import { changePasswordController, deleteUserController, getCurrentUserController, profileUploadController, updateUserController } from "../controllers/user.controller.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
+import upload from "../middlewares/upload.middleware.js";
 
 const router = express.Router();
 
@@ -9,3 +10,4 @@ router.get('/me', authMiddleware, getCurrentUserController);
 router.patch('/me', authMiddleware, updateUserController);
 router.patch('/change-password', authMiddleware, changePasswordController);
 router.delete('/delete-user', authMiddleware, deleteUserController);
+router.patch("/upload-profile", authMiddleware, upload.single('profile'), profileUploadController);
