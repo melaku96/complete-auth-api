7949f2e07b11ea4ec7ceb2fe5dbafed1cc071e14
Melaku Belew <embd2026gc@gmail.com>
2026-05-12 20:03:56 +0300

    implement password changing functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import { changePasswordService } from "../services/user.changepass.service.js";
 import { getMeService } from "../services/user.get.service.js";
 import { updateUserService } from "../services/user.update.service.js";
 import { catchAsync } from "../utils/catchAsync.js";
@@ -20,5 +21,17 @@ export const updateUserController = catchAsync(async(req, res)=>{
   res.status(200).json({
     success: true,
     user
+  });
+});
+
+//change password
+export const changePasswordController = catchAsync(async(req, res)=>{
+  const id = req.user._id;
+  const currentPassword = req.body.password;
+  const newPassword = req.body.newPassword;
+  await changePasswordService(id, currentPassword, newPassword);
+  res.status(200).json({
+    success:true,
+    message: "Your password changed successfully",
   });
 });
